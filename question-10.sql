b442ae061a562d7688c156fc968083d7360f2fc0
Amber Wu <114832146+SirBlank@users.noreply.github.com>
2023-04-05 22:22:49 -0700

    Add files via upload

---
+++ question-10.sql
@@ -0,0 +1,3 @@
+-- (question-10.sql) Put your own information into the database. (You may used falsified information for anything you feel is too personal to share, just make sure your first and last name are the same as what we see in Canvas.)
+INSERT INTO persons
+VALUES('Amber', 'Wu', 'awu8@uw.edu', 'Female', 'Ms', 18, '0 Epic Dub Street', 'Seattle', 'Washington', 98101, '425-111-1111');
