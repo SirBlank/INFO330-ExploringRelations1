ea694882e3119223f503075fcd0decd3d11d2a7a
Amber Wu <114832146+SirBlank@users.noreply.github.com>
2023-04-11 11:17:32 -0700

    Updated after correcting code from TA clarification

--- question-9.sql
+++ question-9.sql
@@ -1,2 +1,2 @@
 -- (question-9.sql) Your instructor has had some bad memories he'd like to purge from the data. Write a single SQL statement to remove anybody who lives in Kansas City (bad experience with BBQ), Pittsburgh (I dislike their football team), or Jacksonville (because Florida) from the database.
-DELETE FROM persons WHERE city = 'Kansas City';
+DELETE FROM persons WHERE city = 'Kansas City' AND 'Pittsburgh' AND 'Jacksonville';
